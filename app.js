e52403a117f15d046826a2141b643dd4b32b645f
LinNanQuan <linnq@zdnst.com>
2017-07-28 11:07:53 +0800

    优化用户权限获取流程

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-//app.js
-const AV = require('./utils/av-weapp-min.js');
+const AV = require('./utils/av-weapp-min.js')
 
 AV.init({
   appId: 'MArHq4pqWa8HeVXdUge3o98t-gzGzoHsz',
@@ -8,29 +7,11 @@ AV.init({
 
 App({
   onLaunch: function () {
-    //调用API从本地缓存中获取数据
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-  },
 
-  getUserInfo: function (cb) {
-    var that = this
-    if (this.globalData.userInfo) {
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    } else {
-      //调用登录接口
-      wx.getUserInfo({
-        withCredentials: false,
-        success: function (res) {
-          that.globalData.userInfo = res.userInfo
-          typeof cb == "function" && cb(that.globalData.userInfo)
-        }
-      })
-    }
   },
 
   globalData: {
+    user: null,
     shareTicket: "",
     updateUserSuccess: false,
     refreshSequenceList: false
--- pages/sequence-list/sequence-list.js
+++ pages/sequence-list/sequence-list.js
@@ -14,14 +14,30 @@ Page({
     */
   onLoad: function () {
     var that = this
-    //获得当前登录用户
+    var app = getApp()
+    // 登录
     util.showLoading()
     AV.User.loginWithWeapp().then(user => {
-      that.data.user = AV.User.current()
+      app.globalData.user = AV.User.current()
       console.log("登录成功")
-      console.log(that.data.user)
+      console.log(app.globalData.user)
+      // 判断用户信息是否已授权
+      wx.getSetting({
+        success: res => {
+          if (!res.authSetting['scope.userInfo']) {
+            return
+          }
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: res => {
+              // 更新用户信息
+              that.updateUserInfo(res.userInfo)
+            }
+          })
+        }
+      })
       that.getMySequences()
-      var shareTicket = getApp().globalData.shareTicket
+      var shareTicket = app.globalData.shareTicket
       if (shareTicket.length > 0) {
         // that.getShareInfo(shareTicket)
       }
@@ -30,6 +46,7 @@ Page({
       console.log("登录失败")
       console.log(error)
     }).catch(console.error)
+
     //可获取转发目标信息
     wx.showShareMenu({
       withShareTicket: true
@@ -50,10 +67,29 @@ Page({
     }
   },
 
+  /**
+  * 更新用户信息
+  */
+  updateUserInfo: function (userInfo) {
+    var app = getApp()
+    var user = AV.User.current()
+    user.set(userInfo).save().then(user => {
+      util.hideLoading()
+      app.globalData.user = AV.User.current()
+      app.globalData.updateUserSuccess = true
+      console.log("更新用户信息成功")
+      console.log(app.globalData.user)
+    }, error => {
+      util.hideLoading()
+      console.log("更新用户信息失败")
+      console.log(error)
+    }).catch(console.error)
+  },
+
   getMySequences: function () {
     util.showLoading()
     var that = this
-    var user = this.data.user
+    var user = getApp().globalData.user
     var userId = user.get("authData").lc_weapp.openid
     // 构建 Sequence 的查询
     var cql = "select * from Sequence where createUserId = '" + userId + "' order by updatedAt desc";
@@ -105,16 +141,20 @@ Page({
   /**
     * 跳转创建接龙页面
     */
-  createSequence: function () {
+  checkUpdateUserInfo: function () {
     var updateUserSuccess = getApp().globalData.updateUserSuccess
     if (updateUserSuccess) {
-      var groupId = this.data.groupId
-      wx.navigateTo({
-        url: '/pages/create-idiom/create-idiom'
-      })
+      this.navigateToCreate()
     } else {
       this.getUserInfo()
     }
+  },
+
+  navigateToCreate: function () {
+    var groupId = this.data.groupId
+    wx.navigateTo({
+      url: '/pages/create-idiom/create-idiom'
+    })
   },
 
   /**
@@ -126,13 +166,14 @@ Page({
     wx.getUserInfo({
       success: (res) => {
         that.updateUserInfo(res.userInfo)
+        that.navigateToCreate()
       },
       fail: (res) => {
         util.hideLoading()
         console.log("获取用户信息失败")
         console.log(res)
         wx.showModal({
-          title: '需要您的授权才可以创建接龙',
+          title: '需要您的授权才能创建接龙',
           showCancel: false,
           success: function (res) {
             wx.openSetting({
@@ -147,29 +188,6 @@ Page({
         })
       }
     })
-  },
+  }
 
-
-  /**
-    * 更新用户信息
-    */
-  updateUserInfo: function (userInfo) {
-    var that = this
-    var user = AV.User.current()
-    user.set(userInfo).save().then(user => {
-      util.hideLoading()
-      console.log("更新用户信息成功")
-      console.log(user)
-      getApp().globalData.updateUserSuccess = true
-      //跳转到创建页
-      var groupId = this.data.groupId
-      wx.navigateTo({
-        url: '/pages/create-idiom/create-idiom'
-      })
-    }, error => {
-      util.hideLoading()
-      console.log("更新用户信息失败")
-      console.log(error)
-    }).catch(console.error)
-  },
 })
